ebf4f2404c1de8c04b497e185502b273c9ff7d70
App Academy Student <email@email.com>
2022-02-28 11:21:37 -0500

    finished hashing

--- hashmap_LRU_cache_project/lib/p02_hashing.rb
+++ hashmap_LRU_cache_project/lib/p02_hashing.rb
@@ -4,11 +4,19 @@ end
 
 class Array
   def hash
+    temp = self.each_with_index.map{|ele, i| ele.hash + i.hash}
+    accumulator = 0
+    return accumulator if self.length == 0
+    temp.inject do |accumulator, ele|
+      accumulator ^ ele
+    end
   end
 end
 
 class String
   def hash
+    #ord is the same as to_i, but for single characters instead of strings
+    self.chars.map {|char| char.ord}.hash
   end
 end
 
@@ -16,6 +24,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort_by {|k,v| k}.hash
   end
 end
